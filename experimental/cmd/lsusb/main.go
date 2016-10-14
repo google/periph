32ce5aa42c14a1e6a36fb558b86df9a256f7f2ff
Marc-Antoine Ruel <maruel@chromium.org>
2016-10-14 18:19:21 -0400

    Make usbhub and lsusb use build tag 'usb'.
    This permits not depending on github.com/kylelemons/gousb/usb by default. This
    is important because this library uses cgo and it depends on a library that is
    usually not installed by default.

--- experimental/cmd/lsusb/main.go
+++ experimental/cmd/lsusb/main.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
+// +build usb
+
 // lsusb prints out information about the USB devices.
 package main
 
