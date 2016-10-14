e0d11c31695e69d6ceffda7658a5021d8f600c1f
M-A <maruel@gmail.com>
2016-10-14 18:28:42 -0400

    Make usbhub and lsusb use build tag 'usb'. (#2)
    This permits not depending on github.com/kylelemons/gousb/usb by default. This
    is important because this library uses cgo and it depends on a library that is
    usually not installed by default.

--- experimental/host/usbbus/doc.go
+++ experimental/host/usbbus/doc.go
@@ -6,4 +6,9 @@
 //
 // This includes handling the connected devices on process startup and handling
 // of the connection events.
+//
+// This package is only built with the build tag 'usb' because it causes a
+// dependency on https://github.com/kylelemons/gousb. This package uses cgo that
+// depends on libusb being installed. This is generally not the case by
+// default, so it causes a go get failure which is really obnoxious to users.
 package usbbus
