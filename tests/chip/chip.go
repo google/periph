8eda8c89a0eb55cfe02a3b3a882332a6ee52af13
Thorsten von Eicken <tve@voneicken.com>
2016-10-22 08:54:34 -0700

    move chip smoke test

--- tests/chip/chip.go
+++ tests/chip/chip.go
@@ -4,65 +4,66 @@
 
 // +build arm
 
-package chip
+package main
 
 import (
+	"fmt"
+	"os"
 	"sort"
-	"testing"
-	"time"
 
 	"github.com/google/pio/conn/gpio"
 	"github.com/google/pio/host"
 	"github.com/google/pio/host/allwinner"
+	"github.com/google/pio/host/chip"
 	"github.com/google/pio/host/headers"
-	"github.com/google/pio/host/sysfs"
 )
 
-// TestChipPresent verifies that CHIP and Allwinner are indeed detected.
-func TestChipPresent(t *testing.T) {
-	if !Present() {
-		t.Fatalf("Did not detect presence of CHIP")
+// testChipPresent verifies that CHIP and Allwinner are indeed detected.
+func testChipPresent() error {
+	if !chip.Present() {
+		return fmt.Errorf("Did not detect presence of CHIP")
 	}
 	if !allwinner.Present() {
-		t.Fatalf("Did not detect presence of Allwinner CPU")
+		return fmt.Errorf("Did not detect presence of Allwinner CPU")
 	}
+	return nil
 }
 
-// TestChipLoading versifies that no error occurs when loading all the drivers for chip.
-func TestChipLoading(t *testing.T) {
+// testChipLoading verifies that no error occurs when loading all the drivers for chip.
+func testChipLoading() error {
 	state, err := host.Init()
 	if err != nil {
-		t.Fatalf("Error loading drivers: %s", err)
+		return fmt.Errorf("Error loading drivers: %s", err)
 	}
 	if len(state.Failed) > 0 {
 		for _, failure := range state.Failed {
-			t.Errorf("%s: %s", failure.D, failure.Err)
+			return fmt.Errorf("%s: %s", failure.D, failure.Err)
 		}
 	}
+	return nil
 }
 
-// TestChipHeaders verifies that the appropriate headers with the right pin count show
+// testChipHeaders verifies that the appropriate headers with the right pin count show
 // up and point checks that a couple of pins are correct.
-func TestChipHeaders(t *testing.T) {
-	host.Init()
+func testChipHeaders() error {
 	h := headers.All()
 	if len(h) != 2 {
-		t.Fatalf("Expected to find 2 headers, not %d\n", len(h))
+		return fmt.Errorf("Expected to find 2 headers, not %d\n", len(h))
 	}
 	if len(h["U13"]) != 20 {
-		t.Errorf("Expected U13 to have 20 rows, not %d\n", len(h["U13"]))
+		return fmt.Errorf("Expected U13 to have 20 rows, not %d\n", len(h["U13"]))
 	}
 	if len(h["U14"]) != 20 {
-		t.Errorf("Expected U13 to have 20 rows, not %d\n", len(h["U13"]))
+		return fmt.Errorf("Expected U13 to have 20 rows, not %d\n", len(h["U13"]))
 	}
 
 	for r := range h["U13"] {
 		if len(h["U13"][r]) != 2 {
-			t.Errorf("Expected row %d of U13 to have 2 pins, not %d\n",
+			return fmt.Errorf("Expected row %d of U13 to have 2 pins, not %d\n",
 				r, len(h["U13"][r]))
 		}
 		if len(h["U14"][r]) != 2 {
-			t.Errorf("Expected row %d of U14 to have 2 pins, not %d\n",
+			return fmt.Errorf("Expected row %d of U14 to have 2 pins, not %d\n",
 				r, len(h["U14"][r]))
 		}
 	}
@@ -76,28 +77,28 @@ func TestChipHeaders(t *testing.T) {
 
 	u13_17 := h["U13"][8][0]
 	if u13_17.Name() != "PD2" {
-		t.Errorf("Expected U13_17 to be PD2, not %s\n", u13_17.Name())
+		return fmt.Errorf("Expected U13_17 to be PD2, not %s\n", u13_17.Name())
 	}
 	p := gpio.ByName("PD2")
 	if p == nil || p.Name() != u13_17.Name() { // p is gpio.PinIO while u13_17 is pins.Pin
-		t.Errorf(`Expected gpio.ByName("PD2") to equal h["U13"][8][0], instead `+
+		return fmt.Errorf(`Expected gpio.ByName("PD2") to equal h["U13"][8][0], instead `+
 			"got %s and %s", p, u13_17)
 	}
 
 	u14_24 := h["U14"][11][1]
 	if p == nil || u14_24.Name() != "PB3" {
-		t.Errorf("Expected U14_24 to be PB3, not %s\n", u14_24.Name())
+		return fmt.Errorf("Expected U14_24 to be PB3, not %s\n", u14_24.Name())
 	}
 
 	u14_17 := h["U14"][8][0]
 	if p == nil || u14_17.Name() != "GPIO1020" {
-		t.Errorf("Expected U14_17 to be GPIO1020, not %s\n", u14_17.Name())
+		return fmt.Errorf("Expected U14_17 to be GPIO1020, not %s\n", u14_17.Name())
 	}
+	return nil
 }
 
-// TestChipGpioNames tests that the gpio pins get the right names.
-func TestChipGpioNames(t *testing.T) {
-	host.Init()
+// testChipGpioNames tests that the gpio pins get the right names.
+func testChipGpioNames() error {
 	all := []string{}
 	for _, p := range gpio.All() {
 		all = append(all, p.Name())
@@ -106,142 +107,146 @@ func TestChipGpioNames(t *testing.T) {
 
 	//t.Log("Pins:", strings.Join(all, ","))
 
-	// must verifies that a pin exists
-	must := func(name string) {
+	must := []string{"PB2", "PE11", "GPIO1022"}
+	for _, name := range must {
 		ix := sort.SearchStrings(all, name)
 		if ix >= len(all) || all[ix] != name {
-			t.Errorf("Expected to find gpio pin %s but it's missing", name)
+			return fmt.Errorf("Expected to find gpio pin %s but it's missing", name)
 		}
 	}
-
-	must("PB2")
-	must("PE11")
-	must("GPIO1022")
+	return nil
 }
 
-// TestChipAliases tests that the various gpio pin aliases get set-up
-func TestChipAliases(t *testing.T) {
-	host.Init()
+// testChipAliases tests that the various gpio pin aliases get set-up
+func testChipAliases() error {
 	tests := map[string]string{ // alias->real
 		"XIO-P4": "GPIO1020", "LCD-D2": "PD2", "GPIO98": "PD2",
 	}
 	for a, r := range tests {
 		p := gpio.ByName(a)
 		if p == nil {
-			t.Errorf("Failed to open %s", a)
-			continue
+			return fmt.Errorf("Failed to open %s", a)
 		}
 		pa, ok := p.(*gpio.PinAlias)
 		if !ok {
-			t.Fatalf("Expected that pin %s is an alias, not %T", a, pa)
-			continue
+			return fmt.Errorf("Expected that pin %s is an alias, not %T", a, pa)
 		}
 		if pa.Name() != a {
-			t.Errorf("The name of alias %s is %s not %s", a, pa.Name(), a)
+			return fmt.Errorf("The name of alias %s is %s not %s", a, pa.Name(), a)
 		}
 		pr, ok := p.(gpio.RealPin)
 		if !ok {
-			t.Errorf("Expected that pin alias %s implement RealPin", a)
-			continue
+			return fmt.Errorf("Expected that pin alias %s implement RealPin", a)
 		}
 		if pr.Real().Name() != r {
-			t.Errorf("Expected that alias %s have real pin %s but it's %s",
+			return fmt.Errorf("Expected that alias %s have real pin %s but it's %s",
 				a, r, pr.Real().Name())
 		}
 	}
+	return nil
 }
 
-// TestChipGpioMem tests two connected pins using memory-mapped gpio
-func TestChipGpioMem(t *testing.T) {
-	host.Init()
-	p1 := pinByName(t, "PB2")
-	p2 := pinByName(t, "PB3")
-	testGpioPair(t, p1, p2)
-	testGpioPair(t, p2, p1)
+func Test() error {
+	tests := []func() error{
+		testChipPresent, testChipLoading, testChipHeaders, testChipGpioNames,
+		testChipAliases,
+	}
+	for _, t := range tests {
+		err := t()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-// TestChipGpioSysfs tests two connected pins using sysfs gpio
-func TestChipGpioSysfs(t *testing.T) {
-	host.Init()
-	p1 := pinByNumber(t, 34)
-	p2 := pinByNumber(t, 35)
-	testGpioPair(t, p1, p2)
-	testGpioPair(t, p2, p1)
+func main() {
+	err := Test()
+	if err != nil {
+		fmt.Printf("CHIP test failed: %s\n", err)
+		os.Exit(1)
+	}
 }
 
-// TestChipGpioXIO tests two connected XIO pins using sysfs gpio
-func TestChipGpioXIO(t *testing.T) {
-	host.Init()
-	p1 := pinByNumber(t, 1022)
-	p2 := pinByNumber(t, 1023)
-	testGpioPair(t, p1, p2)
-	testGpioPair(t, p2, p1)
+/* The following gpio tests are commented out for now in favor of using gpio-test via a shell
+ * script. Once the test startegy settles this can be deleted if it's not used.
+
+// testChipGpioMem tests two connected pins using memory-mapped gpio
+func testChipGpioMem() error {
+	p1, err := pinByName(t, "PB2")
+	if err != nil {
+		return err
+	}
+	p2, err := pinByName(t, "PB3")
+	if err != nil {
+		return err
+	}
+	err = gpio.TestCycle(p1, p2, noPull, false)
+	if err != nil {
+		return err
+	}
+	err = gpio.TestCycle(p2, p1, noPull, false)
+	if err != nil {
+		return err
+	}
+}
+
+// testChipGpioSysfs tests two connected pins using sysfs gpio
+func testChipGpioSysfs() error {
+	p1, err := pinByNumber(t, 34)
+	if err != nil {
+		return err
+	}
+	p2, err := pinByNumber(t, 35)
+	if err != nil {
+		return err
+	}
+	err = gpio.TestCycle(p1, p2, noPull, false)
+	if err != nil {
+		return err
+	}
+	err = gpio.TestCycle(p2, p1, noPull, false)
+	if err != nil {
+		return err
+	}
+}
+
+// testChipGpioXIO tests two connected XIO pins using sysfs gpio
+func testChipGpioXIO() error {
+	p1, err := pinByNumber(t, 1022)
+	if err != nil {
+		return err
+	}
+	p2, err := pinByNumber(t, 1023)
+	if err != nil {
+		return err
+	}
+	err = gpio.TestCycle(p1, p2, noPull, false)
+	if err != nil {
+		return err
+	}
+	err = gpio.TestCycle(p2, p1, noPull, false)
+	if err != nil {
+		return err
+	}
 }
 
 // pinByName gets a gpio pin by name and calls Fatal if it fails
-func pinByName(t *testing.T, name string) gpio.PinIO {
+func pinByName(name string) (gpio.PinIO, error) {
 	p := gpio.ByName(name)
 	if p == nil {
-		t.Fatalf("Failed to open %s", name)
+		return nil, fmt.Errorf("Failed to open %s", name)
 	}
-	return p
+	return p, nil
 }
 
 // pinByNumber gets a *sysfs* pin by number and calls Fatal if it fails
-func pinByNumber(t *testing.T, n int) gpio.PinIO {
+func pinByNumber(n int) (gpio.PinIO, error) {
 	p, err := sysfs.PinByNumber(n)
 	if p == nil {
-		t.Fatalf("Failed to open sysfs(%d): %s", n, err)
+		return nil, fmt.Errorf("Failed to open sysfs(%d): %s", n, err)
 	}
-	return p
+	return p, nil
 }
 
-// testGpioPair checks that output values on p1 are received on p2
-func testGpioPair(t *testing.T, p1, p2 gpio.PinIO) {
-	if err := p2.In(gpio.Float, gpio.None); err != nil {
-		t.Fatalf("Cannot make %s an input: %s", p2, err)
-	}
-	if err := p1.Out(gpio.Low); err != nil {
-		t.Fatalf("Cannot make %s an output: %s", p1, err)
-	}
-	defer p1.In(gpio.Float, gpio.None) // leave pin in a safe state
-
-	// Test simple toggling the output and seeing it on the input.
-	for i := 0; i < 10; i++ {
-		o := gpio.Level(i&1 == 0)
-		p1.Out(o)
-		in := p2.Read()
-		if in != o {
-			t.Fatalf("output %v on %s but read %v on %s", o, p1, in, p2)
-		}
-	}
-
-	// Test edge detection.
-	p1.Out(gpio.Low)
-	if err := p2.In(gpio.Float, gpio.Both); err != nil {
-		t.Errorf("Cannot make %s an input with edge detection: %s", p2, err)
-		return
-	}
-	defer p2.In(gpio.Float, gpio.None) // disable edges again
-	if p2.Read() != gpio.Low {
-		t.Errorf("whoops!")
-	}
-	// Make sure there's no pending edge.
-	for p2.WaitForEdge(time.Millisecond) {
-		p2.Read()
-	}
-	// Toggle a few times.
-	for i := 0; i < 10; i++ {
-		o := gpio.Level(i&1 == 0)
-		p1.Out(o)
-		if edge := p2.WaitForEdge(10 * time.Millisecond); !edge {
-			t.Errorf("output %v on %s but no edge interrupt on %s (input %v, i=%d)",
-				o, p1, p2, p2.Read(), i)
-			return
-		}
-		if in := p2.Read(); in != o {
-			t.Errorf("output %v but read %v", o, in)
-			return
-		}
-	}
-}
+*/
