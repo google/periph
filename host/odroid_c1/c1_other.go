e001de62c84b0d1d37df000db1fca753b3de0d19
Thorsten von Eicken <tve@voneicken.com>
2016-10-23 22:29:58 -0700

    fix copyrights

--- host/odroid_c1/c1_other.go
+++ host/odroid_c1/c1_other.go
@@ -1,4 +1,4 @@
-// Copyright 2016 Thorsten von Eicken. All rights reserved.
+// Copyright 2016 The PIO Authors. All rights reserved.
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
