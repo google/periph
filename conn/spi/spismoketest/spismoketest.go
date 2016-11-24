7788ae618ae2ac27fe9df8229bb76d813b91c91f
Thorsten von Eicken <tve@voneicken.com>
2016-11-23 20:13:13 -0800

    spi smoke test

--- conn/spi/spismoketest/spismoketest.go
+++ conn/spi/spismoketest/spismoketest.go
@@ -8,6 +8,7 @@
 package spismoketest
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -37,6 +38,7 @@ func (s *SmokeTest) Run(args []string) error {
 	busNum := f.Int("bus", -1, "bus number, -1 for lowest numbered bus")
 	csNum := f.Int("cs", -1, "chip select number, -1 for the lowest numbered")
 	wp := f.Int("wp", 0, "gpio pin number for EEPROM write-protect pin")
+	seed := f.Int64("seed", 0, "random number seed, default is to use the time")
 	f.Parse(args)
 
 	// Open the bus.
@@ -49,11 +51,17 @@ func (s *SmokeTest) Run(args []string) error {
 	// Open the WC pin.
 	var wpPin gpio.PinIO
 	if *wp != 0 {
-		wpPin = gpio.ByNumber(*wp)
-		if wpPin == nil {
+		if wpPin = gpio.ByNumber(*wp); wpPin == nil {
 			return fmt.Errorf("spi-smoke: cannot open gpio pin %d for EEPROM write protect", *wp)
 		}
 	}
+
+	// Init rand.
+	if *seed == 0 {
+		*seed = time.Now().UnixNano()
+	}
+	rand.Seed(*seed)
+	log.Printf("spi-smoke: random number seed %d", *seed)
 
 	// Run the tests.
 	if err := s.eeprom(spiDev, wpPin); err != nil {
@@ -77,92 +85,136 @@ func (s *SmokeTest) Run(args []string) error {
 // passed to the test only the completion of reads will be tested.
 //
 // Datasheet: http://www.st.com/content/ccc/resource/technical/document/datasheet/28/42/21/c1/13/bf/47/9a/DM00043274.pdf/files/DM00043274.pdf/jcr:content/translations/en.DM00043274.pdf
-func (s *SmokeTest) eeprom(d spi.Dev, wpPin gpio.PinIO) error {
-	rand.Seed(time.Now().UnixNano())
-
-	// Read byte 10 of the EEPROM and expect to get something.
-	var oneByte [1]byte
-	if err := d.Tx([]byte{0x12}, oneByte[:]); err != nil {
-		return fmt.Errorf("eeprom: error on the first read access")
-	}
-
-	// Read page 5 of the EEPROM and expect to get something.
-	var onePage [16]byte
-	if err := d.Tx([]byte{5 * 16}, onePage[:]); err != nil {
-		return fmt.Errorf("eeprom: error reading page 5")
-	}
-
-	// Stop here if we don't have write-control for the chip.
+func (s *SmokeTest) eeprom(d spi.Conn, wpPin gpio.PinIO) error {
+	// Can't do anything if we don't have write-protect for the chip.
 	if wpPin == nil {
-		log.Println("spi-smoke: no WC pin specified, skipping eeprom write tests")
+		log.Println("spi-smoke: no WP pin specified, skipping eeprom tests")
 		return nil
 	}
 
-	// Enable write-control
+	// Clear write-protect so we can write.
 	if err := wpPin.Out(gpio.High); err != nil {
-		return fmt.Errorf("eeprom: cannot init WC control pin: %s", err)
+		return fmt.Errorf("eeprom: cannot init WP control pin: %s", err)
 	}
-	time.Sleep(time.Millisecond)
-	wpPin.Out(gpio.Low)
-	time.Sleep(time.Millisecond)
-	wpPin.Out(gpio.High)
+
+	var rBuf [35]byte
+	// Read status register of the EEPROM and expect not to get an error (the device
+	// can't produce an error with SPI, but the driver/OS could act up).
+	if err := d.Tx([]byte{cmdReadStatus, 0}, rBuf[:2]); err != nil {
+		return fmt.Errorf("eeprom: error on the first read status access")
+	}
+
+	// Invert one of the block protect bits and expect to read the modified status reg back.
+	sr := 0x02 | ((rBuf[1] & 0x0c) ^ 0x08) // flip BP1
+	d.Tx([]byte{cmdWriteEnable}, rBuf[:1])
+	d.Tx([]byte{cmdWriteStatus, sr}, rBuf[:2])
+	if err := waitReady(d); err != nil {
+		return err
+	}
+	d.Tx([]byte{cmdReadStatus, 0}, rBuf[:2])
+	if (rBuf[1] & 0xc) != (sr & 0x0c) {
+		return fmt.Errorf("eeprom: wrote %#x to status register but got %#x back", sr, rBuf[1])
+	}
+
+	// Clear status register so we can write anywhere.
+	d.Tx([]byte{cmdWriteEnable}, rBuf[:1])
+	d.Tx([]byte{cmdWriteStatus, 0x00}, rBuf[:2])
+	if err := waitReady(d); err != nil {
+		return err
+	}
 
 	// Pick a byte in the first 256 bytes and try to write it and read it back a couple
 	// of times. Using a random byte for "wear leveling"...
-	addr := byte(rand.Intn(256))
+	addr := [2]byte{0, byte(rand.Intn(256))} // 16-bit big-endian address
 	values := []byte{0x55, 0xAA, 0xF0, 0x0F, 0x13}
-	log.Printf("spi-smoke writing&reading EEPROM byte %#x", addr)
+	log.Printf("spi-smoke writing&reading EEPROM byte %#x", addr[1])
 	for _, v := range values {
 		// Write byte.
-		if err := d.Tx([]byte{addr, v}, nil); err != nil {
-			return fmt.Errorf("eeprom: error writing %#x to byte at %#x: %s", v, addr, err)
+		d.Tx([]byte{cmdWriteEnable}, rBuf[:1])
+		d.Tx([]byte{cmdWriteMemory, addr[0], addr[1], v}, rBuf[:4])
+		// Read byte back after the chip is ready.
+		if err := waitReady(d); err != nil {
+			return err
 		}
-		// Read byte back, looping until the chip is ready (it takes a while to complete
-		// the write (5ms max).
-		for {
-			err := d.Tx([]byte{addr}, oneByte[:])
-			if err == nil {
-				break
-			}
-			return fmt.Errorf("eeprom: error reading byte written: %s", err)
+		d.Tx([]byte{cmdReadMemory, addr[0], addr[1], 0}, rBuf[:4])
+		if rBuf[3] != v {
+			return fmt.Errorf("eeprom: wrote %#x but got %#v back", v, rBuf[3])
 		}
 	}
 
 	// Pick a page in the first 256 bytes and try to write it and read it back.
 	// Using a random page for "wear leveling" and randomizing what gets written
 	// so it actually changes from one test run to the next.
-	addr = byte(rand.Intn(256)) & 0xf0 // round to page boundary
-	r := byte(rand.Intn(256))          // randomizer for value written
-	log.Printf("spi-smoke writing&reading EEPROM page %#x with randomizer %#x", addr, r)
+	addr[1] = byte(rand.Intn(256)) & 0xe0 // round to page boundary
+	r := byte(rand.Intn(256))             // randomizer for value written
+	log.Printf("spi-smoke writing&reading EEPROM page %#x", addr)
 	// val calculates the value for byte i.
-	val := func(i int) byte { return byte((i<<4)|(16-i)) ^ r }
-	for i := 0; i < 16; i++ {
+	val := func(i int) byte { return byte((i<<4)|(16-(i>>1))) ^ r }
+	var onePage [32]byte
+	for i := 0; i < 32; i++ {
 		onePage[i] = val(i)
 	}
 	// Write page.
-	if err := d.Tx(append([]byte{addr}, onePage[:]...), nil); err != nil {
-		return fmt.Errorf("eeprom: error writing to page %#x: %s", addr, err)
-	}
-	// Read page back, looping until the chip is ready (it takes a while to complete
-	// the write. Start by zeroing the buffer.
-	for i := 0; i < 16; i++ {
+	d.Tx([]byte{cmdWriteEnable}, rBuf[:1])
+	d.Tx(append([]byte{cmdWriteMemory, addr[0], addr[1]}, onePage[:]...), rBuf[:35])
+	// Zero buffer in anticipation of read.
+	for i := 0; i < 32; i++ {
 		onePage[i] = 0
 	}
-	for {
-		err := d.Tx([]byte{addr}, onePage[:])
-		if err == nil {
-			break
-		}
-		return fmt.Errorf("eeprom: error reading page written: %s", err)
+	// Read page back after the chip is ready.
+	if err := waitReady(d); err != nil {
+		return err
 	}
+	d.Tx(append([]byte{cmdReadMemory, addr[0], addr[1]}, onePage[:]...), rBuf[:35])
 	// Ensure we got the correct data.
-	for i := 0; i < 16; i++ {
-		if onePage[i] != val(i) {
+	for i := 0; i < 32; i++ {
+		if rBuf[i+3] != val(i) {
 			return fmt.Errorf("eeprom: incorrect read of addr %#x: expected %#x got %#x",
-				addr+byte(i), val(i), onePage[i])
+				addr[1]+byte(i), val(i), rBuf[i+3])
 		}
 
+	}
+
+	// Set write-protect, attempt a write, and expect it not to happen.
+	wpPin.Out(gpio.Low)
+	if err := d.Tx([]byte{0x10, 0xA5}, nil); err == nil {
+		return errors.New("eeprom: write with write-control disabled didn't return an error")
+	}
+	// Write the value of the second byte in the just-written page into the first byte.
+	first := rBuf[0]
+	second := rBuf[1]
+	d.Tx([]byte{cmdWriteEnable}, rBuf[:1])
+	d.Tx([]byte{cmdWriteMemory, addr[0], addr[1], second}, rBuf[:4])
+	// Read byte back after the chip is ready.
+	if err := waitReady(d); err != nil {
+		return err
+	}
+	d.Tx([]byte{cmdReadMemory, addr[0], addr[1], 0}, rBuf[:4])
+	if rBuf[3] != first {
+		return fmt.Errorf("eeprom: write protect failed, expected %#x got %#x", first, rBuf[3])
 	}
 
 	return nil
 }
+
+// waitReady reads the status register until the write is complete or a timeout expires.
+func waitReady(d spi.Conn) error {
+	for start := time.Now(); time.Since(start) <= 100*time.Millisecond; {
+		var rBuf [2]byte
+		d.Tx([]byte{cmdReadStatus, 0}, rBuf[:])
+		if rBuf[1]&1 == 0 {
+			return nil
+		}
+	}
+	return errors.New("eeprom: write timout")
+}
+
+// Constants to support EEPROM tests.
+const (
+	cmdWriteEnable  = 0x06
+	cmdWriteDisable = 0x04
+	cmdReadStatus   = 0x05
+	cmdWriteStatus  = 0x01
+	cmdReadMemory   = 0x03
+	cmdWriteMemory  = 0x02
+)
