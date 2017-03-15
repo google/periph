13ec4bf4972638dee5262ace1c5f42dcc6f20d22
Marc-Antoine Ruel <maruel@chromium.org>
2017-03-15 08:57:40 -0400

    Link bcm283x and rpi in arm64 when using host.Init()
    While Raspbian is 32 bits only, there are other distributions for the Raspberry
    Pi 3 that provide a 64 bits userland.

--- host/host_arm64.go
+++ host/host_arm64.go
@@ -7,5 +7,7 @@ package host
 import (
 	// Make sure CPU and board drivers are registered.
 	_ "periph.io/x/periph/host/allwinner"
+	_ "periph.io/x/periph/host/bcm283x"
 	_ "periph.io/x/periph/host/pine64"
+	_ "periph.io/x/periph/host/rpi"
 )
