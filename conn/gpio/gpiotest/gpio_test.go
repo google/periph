0ea9a204536ab0a9a350fb7309d786d5f02edffe
Marc-Antoine Ruel <maruel@chromium.org>
2017-03-24 22:23:00 -0400

    gpiotest: increase coverage to 100%

--- conn/gpio/gpiotest/gpio_test.go
+++ conn/gpio/gpiotest/gpio_test.go
@@ -6,9 +6,70 @@ package gpiotest
 
 import (
 	"testing"
+	"time"
 
 	"periph.io/x/periph/conn/gpio"
 )
+
+func TestPin(t *testing.T) {
+	p := &Pin{N: "GPIO1", Num: 10, Fn: "I2C1_SDA"}
+	if s := p.String(); s != "GPIO1(10)" {
+		t.Fatal(s)
+	}
+	if err := p.In(gpio.PullDown, gpio.NoEdge); err != nil {
+		t.Fatal(err)
+	}
+	if l := p.Read(); l != gpio.Low {
+		t.Fatal(l)
+	}
+	if err := p.In(gpio.PullUp, gpio.NoEdge); err != nil {
+		t.Fatal(err)
+	}
+	if l := p.Read(); l != gpio.High {
+		t.Fatal(l)
+	}
+	if pull := p.Pull(); pull != gpio.PullUp {
+		t.Fatal(pull)
+	}
+	if err := p.Out(gpio.Low); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPin_edge(t *testing.T) {
+	p := &Pin{N: "GPIO1", Num: 1, Fn: "I2C1_SDA", EdgesChan: make(chan gpio.Level)}
+	go func() {
+		p.EdgesChan <- gpio.High
+	}()
+	if !p.WaitForEdge(-1) {
+		t.Fail()
+	}
+	if p.Read() != gpio.High {
+		t.Fail()
+	}
+	if p.WaitForEdge(time.Millisecond) {
+		t.Fail()
+	}
+	go func() {
+		p.EdgesChan <- gpio.Low
+	}()
+	if !p.WaitForEdge(time.Minute) {
+		t.Fail()
+	}
+	if p.Read() != gpio.Low {
+		t.Fail()
+	}
+}
+
+func TestPin_fail(t *testing.T) {
+	p := &Pin{N: "GPIO1", Num: 1, Fn: "I2C1_SDA"}
+	if err := p.PWM(5); err == nil {
+		t.Fatal()
+	}
+	if err := p.In(gpio.Float, gpio.BothEdges); err == nil {
+		t.Fatal()
+	}
+}
 
 func TestAll(t *testing.T) {
 	if 2 != len(gpio.All()) {
