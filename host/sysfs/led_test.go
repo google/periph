e246501303dc9edcddf9f658d733102f6338d7ca
Marc-Antoine Ruel <maruel@chromium.org>
2017-04-09 21:44:51 -0400

    sysfs-led: add basic unit test
    Very partial, need more work.

--- host/sysfs/led_test.go
+++ host/sysfs/led_test.go
@@ -27,8 +27,55 @@ func ExampleLEDByName() {
 	led.Out(gpio.Low)
 }
 
+//
+
 func TestLEDByName(t *testing.T) {
 	if _, err := LEDByName("FOO"); err == nil {
 		t.Fail()
+	}
+}
+
+func TestLED(t *testing.T) {
+	l := LED{number: 42, name: "Glow", root: "/tmp/led/priv/"}
+	if s := l.String(); s != "Glow(42)" {
+		t.Fatal(s)
+	}
+	if s := l.Name(); s != "Glow" {
+		t.Fatal(s)
+	}
+	if n := l.Number(); n != 42 {
+		t.Fatal(n)
+	}
+}
+
+func TestLEDMock(t *testing.T) {
+	l := LED{number: 42, name: "Glow", root: "/tmp/led/priv/"}
+	if s := l.Function(); s != "LED/Off" {
+		t.Fatal(s)
+	}
+	if err := l.In(gpio.PullNoChange, gpio.NoEdge); err != nil {
+		t.Fatal(err)
+	}
+	if l := l.Read(); l != gpio.Low {
+		t.Fatal("need mock")
+	}
+	if err := l.Out(gpio.High); err == nil {
+		t.Fatal("need mock")
+	}
+}
+
+func TestLED_not_supported(t *testing.T) {
+	l := LED{number: 42, name: "Glow", root: "/tmp/led/priv/"}
+	if err := l.In(gpio.PullDown, gpio.NoEdge); err == nil {
+		t.Fatal("sysfs-led no real In() support")
+	}
+	if l.WaitForEdge(-1) {
+		t.Fatal("not supported")
+	}
+	if pull := l.Pull(); pull != gpio.PullNoChange {
+		t.Fatal(pull)
+	}
+	if err := l.PWM(0); err == nil {
+		t.Fatal("not supported")
 	}
 }
