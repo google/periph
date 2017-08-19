5c1baf1ffb50e5ceb83faf0aaa66fc43a030c858
Marc-Antoine Ruel <maruel@chromium.org>
2017-08-19 09:46:00 -0400

    conn: Improve documentation
    Explains the 'Bus → Port → Conn' concept that I had failed to put into words up to now

--- conn/doc.go
+++ conn/doc.go
@@ -4,6 +4,45 @@
 
 // Package conn defines core interfaces for protocols and connections.
 //
-// Subpackages implements all the interfaces that can be used to connects
-// peripherals.
+// Subpackages implements all the supported interfaces that can be used to
+// connects peripherals.
+//
+// Concepts
+//
+// periph uses 3 layered concepts for interfacing:
+//
+//     Bus → Port → Conn
+//
+// Not every subpackage expose all 3 concepts. In fact, most packages don't.
+// For example, SPI doesn't exxpose Bus as the OSes generally only expose the
+// Port, that is, a CS line must be selected right upfront to get an handle.
+// For I²C, there's no Port to configure, so selecting a "slave" address is
+// sufficient to jump directly from a Bus to a Conn.
+//
+// Bus
+//
+// A Bus is a multi-point communication channel where one "master" and multiple
+// "slaves" communicate together. In the case of periph, the Bus handle is
+// assumed to be the "master". The "master" generally initiates communications
+// and selects the "slave" to talk to.
+//
+// As the "master", selecting a "slave" over a Bus converts the Bus into a Port.
+//
+// Examples include SPI, I²C and 1-wire. In each case, selecting a
+// communication line (CS line for SPI, address for I²C or 1-wire) converts the
+// Bus into a Port.
+//
+// Port
+//
+// A port is a point-to-point communication channel that is not initialized and
+// thus, could be used with different parameters: number of bits per word,
+// clock phase, stop bit (for UART), communication speed in Hertz, etc.
+//
+// Configuring a Port converts it into a Conn.
+//
+// Conn
+//
+// A Conn is a fully configured half or full duplex communication channel that
+// is point-to-point, only between two devices. It is ready to use like any
+// readable and/or writable pipe.
 package conn
