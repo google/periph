2b79863285b348c162fdd39fa62185f7d689e69c
Matt Aimonetti <mattaimonetti@gmail.com>
2017-12-16 18:11:09 -0800

    test the constructor function

--- experimental/devices/cap1188/cap1188_test.go
+++ experimental/devices/cap1188/cap1188_test.go
@@ -9,11 +9,13 @@ package cap1188_test
 import (
 	"fmt"
 	"log"
+	"testing"
 	"time"
 
 	"periph.io/x/periph/conn/gpio"
 	"periph.io/x/periph/conn/gpio/gpioreg"
 	"periph.io/x/periph/conn/i2c/i2creg"
+	"periph.io/x/periph/conn/i2c/i2ctest"
 	"periph.io/x/periph/experimental/devices/cap1188"
 )
 
@@ -78,4 +80,49 @@ func Example() {
 	}
 
 	fmt.Print("\n")
+}
+
+func TestNewI2C(t *testing.T) {
+	bus := i2ctest.Playback{
+		Ops: []i2ctest.IO{
+			// chip ID
+			{Addr: 40, W: []byte{0xfd}, R: []byte{0x50}},
+			// clear interrupt
+			{Addr: 40, W: []byte{0x0}, R: []byte{0x0}},
+			{Addr: 40, W: []byte{0x0, 0x0}, R: nil},
+			// enable all inputs
+			{Addr: 40, W: []byte{0x21, 0xff}, R: nil},
+			// enable interrupts
+			{Addr: 40, W: []byte{0x27, 0xff}, R: nil},
+			// enable/disable repeats
+			{Addr: 40, W: []byte{0x28, 0xff}, R: nil},
+			// multitouch
+			{Addr: 40, W: []byte{0x2a, 0x4}, R: nil},
+			// sampling
+			{Addr: 40, W: []byte{0x24, 0x8}, R: nil},
+			// sensitivity
+			{Addr: 40, W: []byte{0x1f, 0x50}, R: nil},
+			// linked leds
+			{Addr: 40, W: []byte{0x72, 0xff}, R: nil},
+			// don't retrigger on hold
+			{Addr: 40, W: []byte{0x28, 0x0}, R: nil},
+			// config
+			{Addr: 40, W: []byte{0x20, 0x30}, R: nil},
+			// config 2
+			{Addr: 40, W: []byte{0x44, 0x61}, R: nil},
+		},
+	}
+	d, err := cap1188.NewI2C(&bus, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s := d.String(); s != "cap1188{playback(40)}" {
+		t.Fatal(s)
+	}
+	if err := d.Halt(); err != nil {
+		t.Fatal(err)
+	}
+	if err := bus.Close(); err != nil {
+		t.Fatal(err)
+	}
 }
