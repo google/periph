ac7e62bc45ae047409dca6627d3a81f4764a1f2a
Gergo Huszty <huszty.gergo@digitaltrip.hu>
2017-12-30 16:01:47 +0100

    Aligning with other BUS lister commands

--- cmd/onewire-list/main.go
+++ cmd/onewire-list/main.go
@@ -9,7 +9,9 @@ import (
 	"fmt"
 	"os"
 
+	"periph.io/x/periph/conn/onewire"
 	"periph.io/x/periph/conn/onewire/onewirereg"
+	"periph.io/x/periph/conn/pin/pinreg"
 	"periph.io/x/periph/host"
 )
 
@@ -18,19 +20,37 @@ func mainImpl() error {
 		return err
 	}
 	for _, ref := range onewirereg.All() {
-		fmt.Println("BUS name:", ref.Name)
+		fmt.Printf("%s", ref.Name)
+		if ref.Number != -1 {
+			fmt.Printf(" #%d", ref.Number)
+		}
+		fmt.Print(":\n")
+		if len(ref.Aliases) != 0 {
+			fmt.Printf("  Aliases:\n")
+			for _, a := range ref.Aliases {
+				fmt.Printf("    %s\n", a)
+			}
+		}
 		bus, err := ref.Open()
 		if err != nil {
 			fmt.Println(" Open error:", err)
 			continue
 		}
+		if p, ok := bus.(onewire.Pins); ok {
+			name, pos := pinreg.Position(p.Q())
+			if name != "" {
+				fmt.Printf("  Q: %-10s found on header %s, #%d\n", p, name, pos)
+			} else {
+				fmt.Printf("  Q: %-10s\n", p)
+			}
+		}
 		addresses, err := bus.Search(false)
 		if err != nil {
-			fmt.Println(" Search error:", err)
+			fmt.Println("  Search error:", err)
 			continue
 		}
 		for _, address := range addresses {
-			fmt.Printf(" Device address: %#016X\n", address)
+			fmt.Printf("  Device address: %#016X\n", address)
 		}
 	}
 	return nil
