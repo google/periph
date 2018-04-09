dfd6a08bbfc4a8f4a32a85554fba8ca7fcb649d1
Marc-Antoine Ruel <maruel@chromium.org>
2018-04-08 21:40:40 -0400

    i2creg: remove dependency on sort
    Use 'sorted insert' instead of 'accumulate then sort'.

--- conn/i2c/i2creg/i2creg_test.go
+++ conn/i2c/i2creg/i2creg_test.go
@@ -5,7 +5,6 @@
 package i2creg
 
 import (
-	"sort"
 	"testing"
 
 	"periph.io/x/periph/conn/i2c"
@@ -59,11 +58,15 @@ func TestAll(t *testing.T) {
 	}
 }
 
-func TestRefList(t *testing.T) {
-	l := refList{&Ref{Name: "b"}, &Ref{Name: "a"}}
-	sort.Sort(l)
-	if l[0].Name != "a" || l[1].Name != "b" {
-		t.Fatal(l)
+func TestRef(t *testing.T) {
+	out := insertRef(nil, &Ref{Name: "b"})
+	out = insertRef(out, &Ref{Name: "d"})
+	out = insertRef(out, &Ref{Name: "c"})
+	out = insertRef(out, &Ref{Name: "a"})
+	for i, l := range []string{"a", "b", "c", "d"} {
+		if out[i].Name != l {
+			t.Fatal(byName)
+		}
 	}
 }
 
