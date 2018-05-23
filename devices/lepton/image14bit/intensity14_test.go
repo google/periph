deea689185b3168e2285e9be75ef95e637c5b44f
Menno Finlay-Smits <inbox@menno.io>
2018-05-23 23:20:11 +1200

    lepton: Add new image14bit subpackage
    This defines a image/draw compatible Gray14 image type and Intensity14
    pixel type. These will soon be used to store the frames return by the
    lepton driver.
    
    This is part of the work for #218.

---
+++ devices/lepton/image14bit/intensity14_test.go
@@ -0,0 +1,28 @@
+// Copyright 2018 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package image14bit
+
+import (
+	"image/color"
+	"testing"
+)
+
+func TestIntensity14(t *testing.T) {
+	if r, g, b, a := Intensity14(16383).RGBA(); r != 65535 || g != r || b != r || a != r {
+		t.Fatal(r, g, b, a)
+	}
+	if r, g, b, a := Intensity14(0).RGBA(); r != 0 || g != r || b != r || a != 65535 {
+		t.Fatal(r, g, b, a)
+	}
+	if Intensity14(16383).String() != "Intensity14(16383)" || Intensity14(0).String() != "Intensity14(0)" {
+		t.Fail()
+	}
+	if Intensity14(8192) != convertIntensity14(Intensity14(8192)) {
+		t.Fatal("failed to convert Intensity14 correctly")
+	}
+	if Intensity14(8224) != convertIntensity14(color.NRGBA{0x80, 0x80, 0x80, 0xFF}) {
+		t.Fatal("failed to convert color.NRGBA correctly")
+	}
+}
