3e7bb619211741cb32b484951c9b94ab9b5d3901
Matthew Hartstonge <matt@mykro.co.nz>
2018-09-28 01:55:29 +1200

    experimental/host: Add  support for mt7688 (#289)
    It contains GPIO declarations that currently forwards to sysfs.

---
+++ experimental/host/mt7688/driver.go
@@ -0,0 +1,38 @@
+// Copyright 2018 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package mt7688
+
+import "errors"
+
+// driverGPIO implements periph.Driver.
+type driverGPIO struct {
+	// gpioMemory is the memory map of the CPU GPIO registers.
+	gpioMemory *gpioMap
+}
+
+func (d driverGPIO) String() string {
+	return "mt7688-gpio"
+}
+
+func (d driverGPIO) Prerequisites() []string {
+	return nil
+}
+
+func (d driverGPIO) After() []string {
+	return []string{"sysfs-gpio"}
+}
+
+func (d *driverGPIO) Init() (bool, error) {
+	if !Present() {
+		return false, errors.New("mt7688 board not detected")
+	}
+
+	// Initialize pins
+	initPins()
+
+	return true, nil
+}
+
+var drvGPIO driverGPIO
