eae6cca7f02788f55641e210fe0f05f9f5a08aa1
Unknown <josh@zool.com>
2018-10-14 13:26:50 +1100

    add more SI units
    add Power as Watt
    add Electrical Capacitance as farad
    add Luminous Flux as lumen
    add Luminous Intensity as candela
    add Energy as joule
    add easy to verify tests for new units and there prefixes(eg 1GJ == GigaJoule etc...)
    add picoAsString to support ElectricalCapacitance
    add picoAsString test

--- conn/physic/units_test.go
+++ conn/physic/units_test.go
@@ -147,6 +147,194 @@ func TestTemperature_String(t *testing.T) {
 	}
 }
 
+func TestPower_String(t *testing.T) {
+	if s := NanoWatt.String(); s != "1nW" {
+		t.Fatalf("%v", s)
+	}
+	if s := MicroWatt.String(); s != "1µW" {
+		t.Fatalf("%v", s)
+	}
+	if s := MilliWatt.String(); s != "1mW" {
+		t.Fatalf("%v", s)
+	}
+	if s := Watt.String(); s != "1W" {
+		t.Fatalf("%v", s)
+	}
+	if s := KiloWatt.String(); s != "1kW" {
+		t.Fatalf("%v", s)
+	}
+	if s := MegaWatt.String(); s != "1MW" {
+		t.Fatalf("%v", s)
+	}
+	if s := GigaWatt.String(); s != "1GW" {
+		t.Fatalf("%v", s)
+	}
+}
+func TestEnergy_String(t *testing.T) {
+	if s := NanoJoule.String(); s != "1nJ" {
+		t.Fatalf("%v", s)
+	}
+	if s := MicroJoule.String(); s != "1µJ" {
+		t.Fatalf("%v", s)
+	}
+	if s := MilliJoule.String(); s != "1mJ" {
+		t.Fatalf("%v", s)
+	}
+	if s := Joule.String(); s != "1J" {
+		t.Fatalf("%v", s)
+	}
+	if s := KiloJoule.String(); s != "1kJ" {
+		t.Fatalf("%v", s)
+	}
+	if s := MegaJoule.String(); s != "1MJ" {
+		t.Fatalf("%v", s)
+	}
+	if s := GigaJoule.String(); s != "1GJ" {
+		t.Fatalf("%v", s)
+	}
+}
+
+func TestCapacitance_String(t *testing.T) {
+	if s := PicoFarad.String(); s != "1pF" {
+		t.Fatalf("%v", s)
+	}
+	if s := NanoFarad.String(); s != "1nF" {
+		t.Fatalf("%v", s)
+	}
+	if s := MicroFarad.String(); s != "1µF" {
+		t.Fatalf("%v", s)
+	}
+	if s := MilliFarad.String(); s != "1mF" {
+		t.Fatalf("%v", s)
+	}
+	if s := Farad.String(); s != "1F" {
+		t.Fatalf("%v", s)
+	}
+	if s := KiloFarad.String(); s != "1kF" {
+		t.Fatalf("%v", s)
+	}
+	if s := MegaFarad.String(); s != "1MF" {
+		t.Fatalf("%v", s)
+	}
+}
+
+func TestLuminousIntensity_String(t *testing.T) {
+	if s := NanoCandela.String(); s != "1ncd" {
+		t.Fatalf("%v", s)
+	}
+	if s := MicroCandela.String(); s != "1µcd" {
+		t.Fatalf("%v", s)
+	}
+	if s := MilliCandela.String(); s != "1mcd" {
+		t.Fatalf("%v", s)
+	}
+	if s := Candela.String(); s != "1cd" {
+		t.Fatalf("%v", s)
+	}
+	if s := KiloCandela.String(); s != "1kcd" {
+		t.Fatalf("%v", s)
+	}
+	if s := MegaCandela.String(); s != "1Mcd" {
+		t.Fatalf("%v", s)
+	}
+	if s := GigaCandela.String(); s != "1Gcd" {
+		t.Fatalf("%v", s)
+	}
+}
+
+func TestFlux_String(t *testing.T) {
+	if s := NanoLumen.String(); s != "1nlm" {
+		t.Fatalf("%v", s)
+	}
+	if s := MicroLumen.String(); s != "1µlm" {
+		t.Fatalf("%v", s)
+	}
+	if s := MilliLumen.String(); s != "1mlm" {
+		t.Fatalf("%v", s)
+	}
+	if s := Lumen.String(); s != "1lm" {
+		t.Fatalf("%v", s)
+	}
+	if s := KiloLumen.String(); s != "1klm" {
+		t.Fatalf("%v", s)
+	}
+	if s := MegaLumen.String(); s != "1Mlm" {
+		t.Fatalf("%v", s)
+	}
+	if s := GigaLumen.String(); s != "1Glm" {
+		t.Fatalf("%v", s)
+	}
+}
+
+func TestPicoAsString(t *testing.T) {
+	data := []struct {
+		in       int64
+		expected string
+	}{
+		{0, "0"}, // 0
+		{1, "1p"},
+		{-1, "-1p"},
+		{900, "900p"},
+		{-900, "-900p"},
+		{999, "999p"},
+		{-999, "-999p"},
+		{1000, "1n"},
+		{-1000, "-1n"},
+		{1100, "1.100n"},
+		{-1100, "-1.100n"}, // 10
+		{999999, "999.999n"},
+		{-999999, "-999.999n"},
+		{1000000, "1µ"},
+		{-1000000, "-1µ"},
+		{1100000, "1.100µ"},
+		{-1100000, "-1.100µ"},
+		{999999501, "1m"},
+		{-999999501, "-1m"},
+		{999999999, "1m"},
+		{-999999999, "-1m"},
+		{1000000000, "1m"},
+		{-1000000000, "-1m"}, // 20
+		{1100000000, "1.100m"},
+		{-1100000000, "-1.100m"},
+		{999999499999, "999.999m"},
+		{-999999499999, "-999.999m"},
+		{999999500001, "1"},
+		{-999999500001, "-1"},
+		{1000000000000, "1"},
+		{-1000000000000, "-1"},
+		{1100000000000, "1.100"},
+		{-1100000000000, "-1.100"},
+		{999999499999999, "999.999"},
+		{-999999499999999, "-999.999"},
+		{999999500000001, "1k"},
+		{-999999500000001, "-1k"},
+		{1000000000000000, "1k"}, //30
+		{-1000000000000000, "-1k"},
+		{1100000000000000, "1.100k"},
+		{-1100000000000000, "-1.100k"},
+		{999999499999999999, "999.999k"},
+		{-999999499999999999, "-999.999k"},
+		{999999500000000001, "1M"},
+		{-999999500000000001, "-1M"},
+		{1000000000000000000, "1M"},
+		{-1000000000000000000, "-1M"},
+		{1100000000000000000, "1.100M"},
+		{-1100000000000000000, "-1.100M"},
+		{-1999499999999999999, "-1.999M"},
+		{1999499999999999999, "1.999M"},
+		{-1999500000000000001, "-2M"},
+		{1999500000000000001, "2M"},
+		{9223372036854775807, "9.223M"},
+		{-9223372036854775807, "-9.223M"},
+		{-9223372036854775808, "-9.223M"},
+	}
+	for i, line := range data {
+		if s := picoAsString(line.in); s != line.expected {
+			t.Fatalf("%d: picoAsString(%d).String() = %s != %s", i, line.in, s, line.expected)
+		}
+	}
+}
+
 func TestNanoAsString(t *testing.T) {
 	data := []struct {
 		in       int64
