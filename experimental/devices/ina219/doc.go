4b6ac9624e0ebad2aca1bc2a107754aee1e10d63
Unknown <josh@zool.com>
2018-10-14 23:04:50 +1100

    "adds ina219 driver : i2c current and voltage sensor"

---
+++ experimental/devices/ina219/doc.go
@@ -0,0 +1,16 @@
+// Copyright 2018 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package ina219 a device driver for an i2c high side current shunt and power
+// monitor ic. Calibration is recommended for accurate current and power
+// measurements. Voltage measurements do not require sensor calibration.
+//
+//
+// Datasheet
+// http://www.ti.com/lit/ds/symlink/ina219.pdf
+//
+// Slave Address:
+// Depending which pins the A1, A0 pins are connected to will change the slave
+// address. Default configuration is address 0x40
+package ina219
