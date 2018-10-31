d84666a541033fb56645a6750cef8ba2079b38dd
alvarowolfx <alvarowolfx@gmail.com>
2018-10-31 11:12:16 -0400

    bh1750: Add support for ambient light sensor

---
+++ experimental/devices/bh1750/doc.go
@@ -0,0 +1,10 @@
+// Copyright 2018 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package bh1750 controls a ROHM BH1750 ambient light sensor, over an i2c bus.
+//
+// Datasheet
+//
+// http://cpre.kmutnb.ac.th/esl/learning/bh1750-light-sensor/bh1750fvi-e_datasheet.pdf
+package bh1750
