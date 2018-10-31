170cd7732ae0d71b56175d28b50f3aff9ba5e4f7
Alvaro Viebrantz <alvarowolfx@gmail.com>
2018-10-31 12:55:39 -0400

    epd: add ePaper display support (#295)

---
+++ experimental/devices/epd/doc.go
@@ -0,0 +1,18 @@
+// Copyright 2018 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package epd controls Waveshare e-paper series displays.
+//
+// More details
+//
+// Datasheets
+//
+// https://www.waveshare.com/w/upload/e/e6/2.13inch_e-Paper_Datasheet.pdf
+//
+// Product page:
+//
+// 2.13 Inch version: https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT
+//
+// 1.54 inch version: https://www.waveshare.com/wiki/1.54inch_e-Paper_Module
+package epd
