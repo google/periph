f4a0782996405529f7f3154f73e6b8df7f448952
Alvaro Viebrantz <alvarowolfx@gmail.com>
2018-10-31 05:46:57 -0400

    ht16k33: add support for LED alphanumeric display (#298)
    Add support to rainbowhat which is a composite of multiple devices.

---
+++ experimental/devices/ht16k33/doc.go
@@ -0,0 +1,16 @@
+// Copyright 2018 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package ht16k33 implements interfacing code to Holtek HT16K33 Alphanumeric 16x8 LED driver.
+//
+// More Details
+//
+// Datasheets
+//
+// http://www.holtek.com/documents/10179/116711/HT16K33v120.pdf
+//
+// Product Page
+//
+// http://www.holtek.com/productdetail/-/vg/HT16K33
+package ht16k33
