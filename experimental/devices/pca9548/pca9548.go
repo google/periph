7b84bec93f6dca65598940f18cbe2aecae461370
Unknown <josh@zool.com>
2018-11-09 22:33:40 +1100

    Refactor to use i2creg.Register
    Add Scan
    Refactor Tests
    Add Scan
    Change port to be unexported.
    Add docs.

--- experimental/devices/pca9548/pca9548.go
+++ experimental/devices/pca9548/pca9548.go
@@ -1,79 +1,149 @@
+// Copyright 2018 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 package pca9548
 
 import (
-	// "periph.io/x/periph/conn/mmr"
 	"errors"
 	"strconv"
 	"sync"
 
 	"periph.io/x/periph/conn/i2c"
+	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/conn/physic"
 )
 
 type Dev struct {
 	c       i2c.Bus
 	address uint16
+	names   []string
 	ports   uint8
-	mu      sync.Mutex
-	port    uint8
+	// mu guards port
+	mu   sync.Mutex
+	port uint8
 }
 
-var DefaultOpts = Opts{Address: 0x00}
+var DefaultOpts = Opts{Address: 0x70, Ports: 8}
 
 type Opts struct {
 	Address uint16
+	Ports   uint8
 }
 
-// New creates a new handel to a pca9548 i2c multiplexer.
-func New(bus i2c.Bus, opts *Opts) (*Dev, error) {
-	return &Dev{
+// Register creates a new handel to a pca9548 I²C multiplexer, and registers
+// port names with the host. These ports can then be used as any other i2c.Bus.
+// The registered port names are in the form: i2cmux/mux-ADD-I where ADD is the
+// multiplexer I²C address in hex and I is the port number.
+// example: "i2cmux-70-0" and "mux-70-0".
+func Register(bus i2c.Bus, opts *Opts) (*Dev, error) {
+	d := &Dev{
 		c:       bus,
-		ports:   8,
 		port:    0xFF,
 		address: opts.Address,
-	}, nil
+		ports:   opts.Ports,
+	}
+
+	for i := uint8(0); i < opts.Ports; i++ {
+		portID := strconv.FormatUint(uint64(i), 10)
+		addrStr := strconv.FormatUint(uint64(opts.Address), 16)
+		name := addrStr + "-" + portID
+		opener := newOpener(d, i)
+		if err := i2creg.Register("i2cmux-"+name, []string{"mux-" + name}, int((opts.Address*10)+uint16(i)), opener); err != nil {
+			return nil, err
+		}
+		d.names = append(d.names, "i2cmux-"+name)
+		d.names = append(d.names, "mux-"+name)
+	}
+	return d, nil
 }
 
-// tx wraps the bus tx, it maintains which port that each bus is registered on
-// os that communication from the master is always on the right port.
-func (d *Dev) tx(port Port, address uint16, w, r []byte) error {
+// ListPortNames lists all the port names registered for the multiplexer.
+func (d *Dev) ListPortNames() []string {
+	return d.names
+}
+
+// Scan the channel for i2c devices, returns a slice of i2c addresses
+func (d *Dev) Scan() ScanList {
+	devices := make(map[string][]uint16)
+	rx := []byte{0x00}
+	addrStr := strconv.FormatUint(uint64(d.address), 16)
+	for port := uint8(0); port < d.ports; port++ {
+		portID := strconv.FormatUint(uint64(port), 10)
+		for address := uint16(1); address < 0x77; address++ {
+			err := d.tx(port, address, nil, rx)
+			if err == nil {
+				devs := devices["mux-"+addrStr+"-"+portID]
+				devs = append(devs, address)
+				devices["mux-"+addrStr+"-"+portID] = devs
+			}
+		}
+	}
+	return devices
+}
+
+type ScanList map[string][]uint16
+
+func (l ScanList) String() string {
+	s := "Scan Results:"
+	for port, addresses := range l {
+		s += "\nPort[" + port + "] " + strconv.FormatInt(int64(len(addresses)), 10) + " found"
+		for _, addr := range addresses {
+			s += "\n\tDevice at 0x" + strconv.FormatUint(uint64(addr), 16)
+		}
+	}
+	return s
+}
+
+// newOpener is a helper for creating an opener func.
+func newOpener(d *Dev, portNumber uint8) i2creg.Opener {
+	return func() (i2c.BusCloser, error) {
+		return &port{
+			mux:    d,
+			number: portNumber,
+		}, nil
+	}
+}
+
+// tx wraps the master bus tx, maintains which port that each bus is registered
+// on so that communication from the master is always on the right port.
+func (d *Dev) tx(port uint8, address uint16, w, r []byte) error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 	if address == d.address {
-		return errors.New("failed to write device address conflicts with mux address")
+		return errors.New("device address conflicts with multiplexer address")
 	}
-	if port.number != d.port {
-		err := d.c.Tx(d.address, []byte{uint8(1 << (port.number))}, nil)
+	// Change active port if needed.
+	if port != d.port {
+		err := d.c.Tx(d.address, []byte{uint8(1 << (port))}, nil)
 		if err != nil {
-			return errors.New("failed to write active channel on mux: " + err.Error())
+			return errors.New("failed to change active port on multiplexer: " + err.Error())
 		}
-		d.port = port.number
+		d.port = port
 	}
 	return d.c.Tx(address, w, r)
 }
 
-// NewBus registers a new i2c bus on the mux.
-func (d *Dev) NewBus(port uint8) (i2c.Bus, error) {
-	if port >= d.ports {
-		return Port{}, errors.New("port number must be between 0 and " + strconv.Itoa(int(d.ports-1)))
-	}
-	return Port{
-		mux:    d,
-		number: port,
-	}, nil
-}
-
 // String gets the port number of the bus on the multiplexer
-func (p Port) String() string { return "Port:" + strconv.Itoa(int(p.number)) }
+func (p *port) String() string { return "Port:" + strconv.Itoa(int(p.number)) }
 
 // SetSpeed is no implemented as the port slaves the master port clock
-func (p Port) SetSpeed(f physic.Frequency) error { return nil }
+func (p *port) SetSpeed(f physic.Frequency) error { return nil }
 
 // Tx does a transaction on the multiplexer port it is register to.
-func (p Port) Tx(addr uint16, w, r []byte) error { return p.mux.tx(p, addr, w, r) }
+func (p *port) Tx(addr uint16, w, r []byte) error { return p.mux.tx(p.number, addr, w, r) }
 
 // Port is a i2c.Bus on the multiplexer
-type Port struct {
-	mux    *Dev
+type port struct {
 	number uint8
+	// mu guards mux
+	mu  sync.Mutex
+	mux *Dev
+}
+
+func (p *port) Close() error {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.mux = nil
+	return nil
 }
--- experimental/devices/pca9548/pca9548_test.go
+++ experimental/devices/pca9548/pca9548_test.go
@@ -1,10 +1,16 @@
+// Copyright 2018 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
 package pca9548
 
 import (
-	"log"
+	"fmt"
 	"testing"
 
+	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/conn/i2c/i2ctest"
+	"periph.io/x/periph/host"
 )
 
 func TestNew(t *testing.T) {
@@ -12,8 +18,13 @@ func TestNew(t *testing.T) {
 		Ops:       nil,
 		DontPanic: true,
 	}
-	d, err := New(bus, &DefaultOpts)
-
+	d, err := Register(bus, &DefaultOpts)
+	defer func() {
+		ports := d.ListPortNames()
+		for _, port := range ports {
+			i2creg.Unregister(port)
+		}
+	}()
 	if err != nil {
 		t.Errorf("don't know how that happened, there was nothing that could fail")
 	}
@@ -26,58 +37,177 @@ func TestNew(t *testing.T) {
 func TestDev_Tx(t *testing.T) {
 	var tests = []struct {
 		initial uint8
-		port    uint8
+		port    string
 		address uint16
 		tx      []i2ctest.IO
+		wantErr bool
 	}{
 		{
-			initial: 0xFF,
-			port:    0,
+			port:    "mux-70-0",
 			address: 0x30,
 			tx: []i2ctest.IO{
-				{Addr: 0x40, W: []byte{0x01}, R: []byte{}},
+				{Addr: 0x70, W: []byte{0x01}, R: []byte{}},
 				{Addr: 0x30, W: []byte{0xAA}, R: []byte{0xBB}},
 			},
 		},
 		{
-			initial: 0,
-			port:    1,
+			port:    "mux-70-1",
 			address: 0x30,
 			tx: []i2ctest.IO{
-				{Addr: 0x40, W: []byte{0x02}, R: []byte{}},
+				{Addr: 0x70, W: []byte{0x02}, R: []byte{}},
 				{Addr: 0x30, W: []byte{0xAA}, R: []byte{0xBB}},
 			},
 		},
 		{
-			initial: 0,
-			port:    7,
+			port:    "mux-70-2",
 			address: 0x30,
 			tx: []i2ctest.IO{
-				{Addr: 0x40, W: []byte{0x80}, R: []byte{}},
+				{Addr: 0x70, W: []byte{0x04}, R: []byte{}},
 				{Addr: 0x30, W: []byte{0xAA}, R: []byte{0xBB}},
 			},
 		},
 		{
-			initial: 2,
-			port:    2,
+			port:    "mux-70-3",
 			address: 0x30,
 			tx: []i2ctest.IO{
+				{Addr: 0x70, W: []byte{0x08}, R: []byte{}},
 				{Addr: 0x30, W: []byte{0xAA}, R: []byte{0xBB}},
 			},
+		},
+		{
+			port:    "mux-70-4",
+			address: 0x30,
+			tx: []i2ctest.IO{
+				{Addr: 0x70, W: []byte{0x10}, R: []byte{}},
+				{Addr: 0x30, W: []byte{0xAA}, R: []byte{0xBB}},
+			},
+		},
+		{
+			port:    "mux-70-5",
+			address: 0x30,
+			tx: []i2ctest.IO{
+				{Addr: 0x70, W: []byte{0x20}, R: []byte{}},
+				{Addr: 0x30, W: []byte{0xAA}, R: []byte{0xBB}},
+			},
+		},
+		{
+			port:    "mux-70-6",
+			address: 0x30,
+			tx: []i2ctest.IO{
+				{Addr: 0x70, W: []byte{0x40}, R: []byte{}},
+				{Addr: 0x30, W: []byte{0xAA}, R: []byte{0xBB}},
+			},
+		},
+		{
+			port:    "mux-70-7",
+			address: 0x30,
+			tx: []i2ctest.IO{
+				{Addr: 0x70, W: []byte{0x80}, R: []byte{}},
+				{Addr: 0x30, W: []byte{0xAA}, R: []byte{0xBB}},
+			},
+		},
+		{
+			port:    "mux-70-0",
+			address: 0x70,
+			tx:      []i2ctest.IO{},
+			wantErr: true,
+		},
+		{
+			port:    "mux-70-0",
+			address: 0x30,
+			tx: []i2ctest.IO{
+				{Addr: 0x70, W: []byte{}, R: []byte{}},
+			},
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
+
 		bus := &i2ctest.Playback{
 			Ops:       tt.tx,
 			DontPanic: true,
 		}
-		d := &Dev{c: bus, address: 0x40, port: tt.initial}
-		p := Port{d, tt.port}
+		host.Init()
 
-		err := p.Tx(tt.address, []byte{0xAA}, []byte{0xBB})
+		d, err := Register(bus, &DefaultOpts)
 		if err != nil {
-			log.Println(err)
+			t.Errorf("failed to open I²C: %v", err)
 		}
 
+		bus0, err := i2creg.Open(tt.port)
+		if err != nil {
+			t.Errorf("failed to open I²C: %v", err)
+		}
+		defer bus0.Close()
+		err = bus0.Tx(tt.address, []byte{0xAA}, []byte{0xBB})
+
+		if err != nil && !tt.wantErr {
+			t.Errorf("expected no error but got: %v", err)
+		}
+		if err == nil && tt.wantErr {
+			t.Errorf("expected error")
+		}
+		ports := d.ListPortNames()
+		for _, port := range ports {
+			i2creg.Unregister(port)
+		}
 	}
+}
+
+func TestScanList_String(t *testing.T) {
+	var sl ScanList = make(map[string][]uint16)
+	sl["mux-70-1"] = []uint16{0x49}
+
+	expected := "Scan Results:\nPort[mux-70-1] 1 found\n" +
+		"\tDevice at 0x49"
+
+	got := sl.String()
+	if got != expected {
+		t.Errorf("expected: \n%v but got: \n%v", expected, got)
+	}
+}
+
+func Test_port_String(t *testing.T) {
+	p := port{
+		number: 6,
+	}
+	expected := "Port:6"
+
+	got := p.String()
+	if got != expected {
+		t.Errorf("expected: \n%v but got: \n%v", expected, got)
+	}
+}
+
+func TestDev_Scan(t *testing.T) {
+	tx := []i2ctest.IO{
+		{Addr: 0x70, W: []byte{0x01}, R: []byte{}}, // select port
+		{Addr: 0x01, W: []byte{}, R: []byte{0x00}}, // ack
+		{Addr: 0x02, W: []byte{}, R: []byte{0x00}},
+		{Addr: 0x03, W: []byte{}, R: []byte{0x00}},
+		{Addr: 0x04, W: []byte{}, R: []byte{0x00}},
+	}
+	bus := &i2ctest.Playback{
+		Ops:       tx,
+		DontPanic: true,
+	}
+	host.Init()
+
+	d, err := Register(bus, &DefaultOpts)
+	if err != nil {
+		t.Errorf("failed to open I²C: %v", err)
+	}
+
+	sl := d.Scan()
+	fmt.Printf("%+#v", sl)
+
+	if len(sl["mux-70-0"]) == 0 {
+		t.Errorf("expected list not to be empty")
+	}
+
+	ports := d.ListPortNames()
+	for _, port := range ports {
+		i2creg.Unregister(port)
+	}
+
 }
