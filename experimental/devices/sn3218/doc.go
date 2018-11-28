b619643cf1f814a67a52a9265616e8839cc448e8
Martin Leuschner <martinleuschner@googlemail.com>
2018-11-28 21:29:03 +0000

    fix https://github.com/google/periph/pull/348#discussion_r236506277

--- experimental/devices/sn3218/doc.go
+++ experimental/devices/sn3218/doc.go
@@ -1,11 +1,12 @@
 // Copyright 2018 The Periph Authors. All rights reserved.
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
-//
+
 // Package sn3218 controls a SN3218 LED driver with 18 LEDs over an i2c bus.
 // See cmd/sn3218/main.go for a usage example. Make sure to run Reset() after
 // New(), so that the state of the LEDs match the register of the chip.
 //
 // Datasheet
-// https://github.com/pimoroni/sn3218/blob/master/datasheets/sn3218-datasheet.pdf
+//
+// http://www.si-en.com/uploadpdf/s2011517171720.pdf
 package sn3218
