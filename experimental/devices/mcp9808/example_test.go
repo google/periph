28015ea4bdbc9c7ab55acfabee4f8ab7c4f9707d
Unknown <josh@zool.com>
2018-12-18 16:49:24 +1100

    Add examples

--- experimental/devices/mcp9808/example_test.go
+++ experimental/devices/mcp9808/example_test.go
@@ -8,12 +8,14 @@ import (
 	"fmt"
 	"log"
 
+	"periph.io/x/periph/conn/physic"
+
 	"periph.io/x/periph/conn/i2c/i2creg"
 	"periph.io/x/periph/experimental/devices/mcp9808"
 	"periph.io/x/periph/host"
 )
 
-func Example() {
+func ExampleDev_Sense() {
 	// Make sure periph is initialized.
 	if _, err := host.Init(); err != nil {
 		log.Fatal(err)
@@ -26,7 +28,7 @@ func Example() {
 	}
 	defer bus.Close()
 
-	// Create a new power sensor.
+	// Create a new temperature sensor.
 	sensor, err := mcp9808.New(bus, &mcp9808.DefaultOpts)
 	if err != nil {
 		log.Fatalln(err)
@@ -40,4 +42,43 @@ func Example() {
 	}
 
 	fmt.Println(measurement)
+}
+
+func ExampleDev_SenseWithAlerts() {
+	// Make sure periph is initialized.
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Open default I²C bus.
+	bus, err := i2creg.Open("")
+	if err != nil {
+		log.Fatalf("failed to open I²C: %v", err)
+	}
+	defer bus.Close()
+
+	// Create a new temperature sensor.
+	sensor, err := mcp9808.New(bus, &mcp9808.DefaultOpts)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	lower := physic.ZeroCelsius
+	upper := physic.ZeroCelsius + 25*physic.Celsius
+	critical := physic.ZeroCelsius + 32*physic.Celsius
+
+	// Read values from sensor.
+	temperature, alerts, err := sensor.SenseWithAlerts(lower, upper, critical)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if len(alerts) > 0 {
+		for _, alert := range alerts {
+			fmt.Println(alert)
+		}
+	}
+
+	fmt.Println(temperature)
 }
