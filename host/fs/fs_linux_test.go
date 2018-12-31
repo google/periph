b880e411e727f59153494f1182c07e6c9e813e50
M-A <maruel@gmail.com>
2018-12-31 11:17:27 -0500

    fs: add new epoll loop to fix issue 323 (#367)
    The new epoll loop has the following properties:
    - It uses a single blocking OS thread, independent of the number of
      edges watched.
    - Notes the timestamp at which each event fires.
    
    Includes unit tests that are testing on OS pipes and sockets, as we
    can't assume there's GPIO sysfs handles available (e.g. travis). This
    increases code coverage to 70% on linux.
    
    Small doc update to Ioctler.
    
    No externally visible change yet.
    
    Doesn't import context yet, it will be used by pieces building on top of
    eventListeners.

--- host/fs/fs_linux_test.go
+++ host/fs/fs_linux_test.go
@@ -5,7 +5,11 @@
 package fs
 
 import (
+	"io/ioutil"
+	"net"
+	"os"
 	"testing"
+	"time"
 )
 
 func TestEpollEvent_String(t *testing.T) {
@@ -17,5 +21,209 @@ func TestEpollEvent_String(t *testing.T) {
 	}
 	if s := epollEvent(0).String(); s != "0" {
 		t.Fatal(s)
+	}
+}
+
+func TestAddFd_Zero(t *testing.T) {
+	// We assume this is a bad file descriptor.
+	ev, cancelEv := getListener(t)
+	defer cancelEv()
+
+	const flags = epollET | epollPRI
+	if err := ev.addFd(0xFFFFFFFF, make(chan time.Time), flags); err == nil || err.Error() != "bad file descriptor" {
+		t.Fatal("expected failure", err)
+	}
+}
+
+func TestAddFd_File(t *testing.T) {
+	// listen cannot listen to a file.
+	ev, cancelEv := getListener(t)
+	defer cancelEv()
+
+	f, err := ioutil.TempFile("", "periph_fs")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := os.Remove(f.Name()); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	const flags = epollET | epollPRI
+	if err := ev.addFd(f.Fd(), make(chan time.Time), flags); err == nil || err.Error() != "operation not permitted" {
+		t.Fatal("expected failure", err)
+	}
+}
+
+func TestListen_Pipe(t *testing.T) {
+	start := time.Now()
+	ev, cancelEv := getListener(t)
+	defer cancelEv()
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatal(err)
+	}
+	c := make(chan time.Time)
+	// Pipes do not support epollPRI, so use epollIN instead.
+	const flags = epollET | epollIN
+	if err := ev.addFd(r.Fd(), c, flags); err != nil {
+		t.Fatal(err)
+	}
+
+	// Produce a single event.
+	if _, err := w.Write([]byte("foo")); err != nil {
+		t.Fatal(err)
+	}
+	expectChan(t, c, start)
+	notExpectChan(t, c, "should have produced a single event")
+
+	// Produce one or two events.
+	if _, err := w.Write([]byte("bar")); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := w.Write([]byte("baz")); err != nil {
+		t.Fatal(err)
+	}
+	expectChan(t, c, start)
+	// It's a race condition between EpollWait() and reading back from the
+	// channel.
+	select {
+	case <-c:
+	default:
+	}
+
+	if err := ev.removeFd(r.Fd()); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestListen_Socket(t *testing.T) {
+	start := time.Now()
+	ev, _ := getListener(t)
+
+	ln, err := net.ListenTCP("tcp4", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := ln.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	conn, err := net.DialTCP("tcp4", nil, ln.Addr().(*net.TCPAddr))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := conn.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	recv, err := ln.Accept()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := recv.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	f, err := recv.(*net.TCPConn).File()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// This channel needs to be buffered since there's going to be an even
+	// immediately triggered.
+	c := make(chan time.Time, 1)
+	// TODO(maruel): Sockets do support epollPRI on out-of-band data. This would
+	// make this test a bit more similar to testing a GPIO sysfs file descriptor.
+	const flags = epollET | epollIN
+	if err := ev.addFd(f.Fd(), c, flags); err != nil {
+		t.Fatal(err)
+	}
+	notExpectChan(t, c, "starting should not produce an event")
+
+	// Produce one or two events.
+	// It's a race condition between EpollWait() and reading back from the
+	// channel.
+	if _, err := conn.Write([]byte("bar\n")); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := conn.Write([]byte("baz\n")); err != nil {
+		t.Fatal(err)
+	}
+	expectChan(t, c, start)
+	// It's a race condition between EpollWait() and reading back from the
+	// channel.
+	select {
+	case <-c:
+	default:
+	}
+
+	// Empty the buffer.
+	var buf [16]byte
+	expected := "bar\nbaz\n"
+	if n, err := recv.Read(buf[:]); n != len(expected) || err != nil {
+		t.Fatal(n, err)
+	}
+	if s := string(buf[:len(expected)]); s != expected {
+		t.Fatal(s)
+	}
+
+	// Produce one event.
+	if _, err := conn.Write([]byte("foo\n")); err != nil {
+		t.Fatal(err)
+	}
+	expectChan(t, c, start)
+	notExpectChan(t, c, "should have produced a single event")
+
+	if err := ev.removeFd(f.Fd()); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestWakeUpLoop(t *testing.T) {
+	// Make sure it doesn't hang when the loop is not running.
+	ev := &eventsListener{}
+	ev.wakeUpLoop(nil)
+}
+
+//
+
+// getListener returns a preinitialized eventsListenenr
+func getListener(t *testing.T) (*eventsListener, func()) {
+	ev := &eventsListener{}
+	if err := ev.init(); err != nil {
+		t.Fatal(err)
+	}
+	return ev, func() {
+		if err := ev.stopLoop(); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func expectChan(t *testing.T, c <-chan time.Time, start time.Time) {
+	select {
+	case v := <-c:
+		if v.Before(start) {
+			t.Fatal("received an timestamp that was too early", v, start)
+		}
+	case <-time.After(5 * time.Second):
+		t.Fatal("timed out after 5 seconds, waiting for an event")
+	}
+}
+
+func notExpectChan(t *testing.T, c <-chan time.Time, errmsg string) {
+	select {
+	case <-c:
+		t.Fatal(errmsg)
+	default:
 	}
 }
