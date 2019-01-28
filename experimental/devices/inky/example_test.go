67d48660f27d886418aed75d6a1966eb0ac2c558
John Maguire <john.maguire@gmail.com>
2019-01-28 17:59:04 +0000

    Add example for inky pHAT

---
+++ experimental/devices/inky/example_test.go
@@ -0,0 +1,54 @@
+package inky_test
+
+import (
+	"flag"
+	"image/png"
+	"log"
+	"os"
+
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/conn/spi/spireg"
+	"periph.io/x/periph/experimental/devices/inky"
+	"periph.io/x/periph/host"
+)
+
+var path = flag.String("image", "", "Path to image file (212x104) to display")
+
+func Example() {
+	flag.Parse()
+
+	f, err := os.Open(*path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	img, err := png.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := spireg.Open("SPI0.0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dc := gpioreg.ByName("22")
+	reset := gpioreg.ByName("27")
+	busy := gpioreg.ByName("17")
+
+	dev, err := inky.New(b, dc, reset, busy, &inky.Opts{
+		Model:       inky.PHAT,
+		ModelColor:  inky.Red,
+		BorderColor: inky.Black,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dev.Draw(img.Bounds(), img, image.ZP)
+}
