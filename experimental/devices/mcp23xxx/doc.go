737d2588500ee2c89e6d43939fa39d3bd555474b
Balázs Grill <balazs.grill@live.com>
2020-03-02 09:02:19 +0100

    MCP23xxx: Added copyright comments

--- experimental/devices/mcp23xxx/doc.go
+++ experimental/devices/mcp23xxx/doc.go
@@ -1,8 +1,8 @@
-// Copyright 2018 The Periph Authors. All rights reserved.
+// Copyright 2020 The Periph Authors. All rights reserved.
 // Use of this source code is governed under the Apache License, Version 2.0
 // that can be found in the LICENSE file.
 
-// Package mcp23xxx controls IO extenders belonging to Microchip MCP23 family
+// Package mcp23xxx provides driver for the MCP23 family of IO extenders
 //
 // Supported variants:
 // * MCP23008/MCP23S08
--- experimental/devices/mcp23xxx/pins.go
+++ experimental/devices/mcp23xxx/pins.go
@@ -1,3 +1,8 @@
+// Copyright 2020 The Periph Authors. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Package mcp23xxx provides driver for the MCP23 family of IO extenders
 package mcp23xxx
 
 import (
