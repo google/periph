f888f759cf844a25d7ce5d5530f48364df52ee57
mhabb <martin.habbecke@gmail.com>
2020-07-27 11:14:05 -0700

    netlink/onewire: fix error handling when reading from the netlink socket (#448)
    When responding to a sequence of 1-wire commands, the kernel driver
    individually acks every single one. It is important to keep reading all
    of these messages in case of an error in order to flush the netlink
    socket for future commands.

--- experimental/host/netlink/onewire.go
+++ experimental/host/netlink/onewire.go
@@ -282,20 +282,29 @@ func (ws *w1Socket) sendAndRecv(seq uint32, m *w1Msg) ([]byte, error) {
 	}
 
 	var data []byte
+	var err1 error
+
+	// Read responses and acks for all commands. It is important to keep
+	// reading in case of an error to flush the netlink socket for future
+	// commands.
 	for _, cmd := range m.cmds {
 		if cmd.wantResponse {
 			d, err := ws.recvCmd(seq, seq+1, m.typ, cmd.typ)
-			if err != nil {
-				return nil, fmt.Errorf("failed to receive command payload: %v", err)
+			if err1 == nil {
+				err1 = err
 			}
 			data = d
 		}
 
 		// Every command is ack'ed with a separate message, including commands
-		// for which a response has already been returned.
-		if _, err := ws.recvCmd(seq, 0, m.typ, cmd.typ); err != nil {
-			return nil, fmt.Errorf("failed to receive command ack message: %v", err)
+		// for which a response has already been received.
+		if _, err := ws.recvCmd(seq, 0, m.typ, cmd.typ); err1 == nil {
+			err1 = err
 		}
+	}
+
+	if err1 != nil {
+		return nil, fmt.Errorf("failed to receive response / acks: %v", err1)
 	}
 
 	return data, nil
